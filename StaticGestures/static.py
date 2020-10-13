5f183e9ccc54682855d1d1826a3f0fffe4e8c8e6
rishabhpar <rishabh.parekh@utexas.edu>
2020-10-12 20:22:24 -0500

    add pics

---
+++ StaticGestures/static.py
@@ -0,0 +1,27 @@
+import cv2 
+import numpy as np 
+import matplotlib.pyplot as plt
+
+##### Le Plan ######
+# gestures:
+# 0 -> white
+# 1 -> blue
+# 2 -> green
+# 3 -> yellow
+# 4 -> red
+# 5 -> black
+
+# dataset pulled from kaggle 
+# https://www.kaggle.com/niksanjp/number-of-fingers?
+
+
+# train a model on these 6 numbers for classification
+
+# dataflow:
+# opencv using webcam -> RGB image -> classify -> output
+# https://medium.com/analytics-vidhya/sign-language-recognition-using-cnn-and-opencv-beginner-level-72091ca35a19
+
+
+
+
+
