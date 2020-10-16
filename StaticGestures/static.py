f9f9071a12732c15d53de48f94b7270c134a3301
Lindia Tjuatja <lindia.tjuatja@gmail.com>
2020-10-15 22:05:31 -0500

    live share session 1

--- StaticGestures/static.py
+++ StaticGestures/static.py
@@ -21,7 +21,32 @@ import matplotlib.pyplot as plt
 # opencv using webcam -> RGB image -> classify -> output
 # https://medium.com/analytics-vidhya/sign-language-recognition-using-cnn-and-opencv-beginner-level-72091ca35a19
 
+# 1. get the camera working to see our hand gesture
+# 2. identify the region with our hand and extract
+# 3. train model with the gajjilion images from kaggle
+# 4. use model to predict which class of gesture the extract cv image belongs to 
+
+def crop_image(image, x, y, width, height):
+    return image[y:y + height, x:x + width]
 
 
+while True:  
+    # capturing the image from webcam 
+    cam_capture = cv2.VideoCapture(0)
+    _, image_frame = cam_capture.read()
 
+    # to crop required part
+    im2 = crop_image(image_frame, 300,300,300,300)
+    
+    # convert to grayscale 
+    image_grayscale = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
 
+    # blurring the image   
+    image_grayscale_blurred =cv2.GaussianBlur(image_grayscale, (15,15), 0)
+
+    # resize the image to 28x28
+    im3 = cv2.resize(image_grayscale_blurred, (28,28), interpolation = cv2.INTER_AREA)
+
+    # expand the dimensions from 28x28 to 1x28x28x1
+    im4 = np.resize(im3, (28, 28, 1))
+    im5 = np.expand_dims(im4, axis=0)   
