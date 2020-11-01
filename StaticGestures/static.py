8cfe1302f7576bf5a0f70af7ced9e24d8e74e82a
rishabhpar <rishabh.parekh@utexas.edu>
2020-11-01 15:44:17 -0600

    binarized images

--- StaticGestures/static.py
+++ StaticGestures/static.py
@@ -2,6 +2,25 @@ import cv2
 import numpy as np 
 import matplotlib.pyplot as plt
 
+
+
+# img = cv2.imread('fingers/train/gradient.png',0)
+# ret,thresh1 = cv2.threshold(img,127,255,cv2.THRESH_BINARY)
+# ret,thresh2 = cv2.threshold(img,127,255,cv2.THRESH_BINARY_INV)
+# ret,thresh3 = cv2.threshold(img,127,255,cv2.THRESH_TRUNC)
+# ret,thresh4 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO)
+# ret,thresh5 = cv2.threshold(img,127,255,cv2.THRESH_TOZERO_INV)
+
+# titles = ['Original Image','BINARY','BINARY_INV','TRUNC','TOZERO','TOZERO_INV']
+# images = [img, thresh1, thresh2, thresh3, thresh4, thresh5]
+
+# for i in xrange(6):
+#     plt.subplot(2,3,i+1),plt.imshow(images[i],'gray')
+#     plt.title(titles[i])
+#     plt.xticks([]),plt.yticks([])
+
+# plt.show()
+
 ##### Le Plan ######
 # gestures:
 # 0 -> white
@@ -34,27 +53,27 @@ import matplotlib.pyplot as plt
 # 4. background subtraction
 # 5. threshold
 
-def crop_image(image, x, y, width, height):
-    return image[y:y + height, x:x + width]
+# def crop_image(image, x, y, width, height):
+#     return image[y:y + height, x:x + width]
 
 
-while True:  
-    # capturing the image from webcam 
-    cam_capture = cv2.VideoCapture(0)
-    _, image_frame = cam_capture.read()
+# while True:  
+#     # capturing the image from webcam 
+#     cam_capture = cv2.VideoCapture(0)
+#     _, image_frame = cam_capture.read()
 
-    # to crop required part
-    im2 = crop_image(image_frame, 300,300,300,300)
+#     # to crop required part
+#     im2 = crop_image(image_frame, 300,300,300,300)
     
-    # convert to grayscale 
-    image_grayscale = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
+#     # convert to grayscale 
+#     image_grayscale = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
 
-    # blurring the image   
-    image_grayscale_blurred =cv2.GaussianBlur(image_grayscale, (15,15), 0)
+#     # blurring the image   
+#     image_grayscale_blurred =cv2.GaussianBlur(image_grayscale, (15,15), 0)
 
-    # resize the image to 28x28
-    im3 = cv2.resize(image_grayscale_blurred, (28,28), interpolation = cv2.INTER_AREA)
+#     # resize the image to 28x28
+#     im3 = cv2.resize(image_grayscale_blurred, (28,28), interpolation = cv2.INTER_AREA)
 
-    # expand the dimensions from 28x28 to 1x28x28x1
-    im4 = np.resize(im3, (28, 28, 1))
-    im5 = np.expand_dims(im4, axis=0)   
+#     # expand the dimensions from 28x28 to 1x28x28x1
+#     im4 = np.resize(im3, (28, 28, 1))
+#     im5 = np.expand_dims(im4, axis=0)   
