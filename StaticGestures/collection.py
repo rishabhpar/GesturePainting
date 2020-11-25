4287b22ecacd13edb1ceb984c979825df000aa47
rishabhpar <rishabh.parekh@utexas.edu>
2020-11-24 22:13:25 -0600

    better fingers

--- StaticGestures/collection.py
+++
@@ -1,5 +0,0 @@
-import sys
-import os
-import cv2
-from PIL import Image
-
--- StaticGestures/findHand.py
+++ StaticGestures/findHand.py
@@ -8,22 +8,6 @@ import os
 from matplotlib import pyplot as plt
 from collections import Counter
 from PIL import Image
-from tensorflow import keras
-model = keras.models.load_model("model_1.h5")
-# import torch
-# import torch.nn as nn
-# from torchvision import *
-# import torchvision.transforms as transforms
-
-# model = models.resnet50(pretrained=True)
-# # Create the model and change the dimension of the output
-# num_ftrs = model.fc.in_features
-# model.fc = nn.Linear(num_ftrs, 128)
-# # Load the pre-trained model, which has old_num_classes
-# model.load_state_dict(torch.load('resnet.pth', map_location='cpu'))
-# # Now change the model to new_num_classes
-# model.fc = nn.Linear(num_ftrs, 128)
-# # Done
 
 # parameters
 cap_region_x_begin=0.45  # start point/total width
@@ -32,11 +16,11 @@ threshold = 70  #  BINARY threshold
 blurValue = 5  # GaussianBlur parameter
 bgSubThreshold = 50
 learningRate = 0
+count = 0
 
 # variables
 isBgCaptured = 0   # bool, whether the background captured
 isFistCaptured = 0
-triggerSwitch = False  # if true, keyborad simulator works
 
 def removeBG(frame):
     fgmask = bgModel.apply(frame,learningRate=learningRate)
@@ -69,13 +53,8 @@ def calculateFingers(res,drawing):  # -> finished bool, cnt: finger count
                 if angle <= math.pi / 2:  # angle less than 90 degree, treat as fingers
                     cnt += 1
                     cv2.circle(drawing, far, 8, [211, 84, 0], -1)
-            #print(cnt)
-            return True, cnt
-    return False, 0
-
-
-    
-    
+            return True, cnt+1
+    return False, -1
 
 # Camera
 camera = cv2.VideoCapture(0)
@@ -105,9 +84,6 @@ while camera.isOpened():
         blur = cv2.GaussianBlur(gray, (blurValue, blurValue), 0) # blur for better binarization
         cv2.imshow('blur', blur)
         ret, thresh = cv2.threshold(blur, threshold, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU) # binarize
-        # cv2.imshow('binary', thresh)
-        # print("There are this many fingers: " + get_name(thresh))
-
         im_floodfill = thresh.copy()
         h, w = thresh.shape[:2]
         mask = np.zeros((h+2, w+2), np.uint8)
@@ -118,61 +94,31 @@ while camera.isOpened():
 
         cv2.imshow('binary', img_bin)
 
-        ##########   DATA COLLECTION ################
-
-        #Convert the captured frame into RGB
-        im = Image.fromarray(img_bin.astype(np.uint8), 'L')
-
-        #Resizing into 128x128 because we trained the model with this image size.
-        im = im.resize((128,128))
-        img_array = np.array(im)
-
-        #Our keras model used a 4D tensor, (images x height x width x channel)
-        #So changing dimension 128x128x3 into 1x128x128x3 
-        img_array = np.expand_dims(img_array, axis=0)
-        img_array = np.expand_dims(img_array, axis=-1)
-
-        #Calling the predict method on model to predict 'me' on the image
-        prediction = model.predict(img_array)
-        pred = np.argmax(prediction[0])
-        print(pred)
-
-
-
-        # im = Image.fromarray(img_bin.astype(np.uint8))
-        # # cv2.imshow('saved', np.array(im))
-        # # filename += 1
-        # # count += 1
-        # im = im.resize((128,128))
-        # # im.save(os.path.join(directory, str(filename)+"_5.jpg"), "JPEG") #change
-
-
-
         # get the coutours
-        # thresh1 = copy.deepcopy(thresh)
-        # contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-        # contours = max(contours, key=lambda x: cv2.contourArea(x))
-        # cv2.drawContours(thresh1, [contours], -1, (255,255,0), 2)
-        # cv2.imshow("contours", thresh1)
-        # length = len(contours)
-        # maxArea = -1
-        # if length > 0:
-        #     for i in range(length):  # find the biggest contour (according to area)
-        #         temp = contours[i]
-        #         area = cv2.contourArea(temp)
-        #         if area > maxArea:
-        #             maxArea = area
-        #             ci = i
+        thresh1 = copy.deepcopy(thresh)
+        contours, hierarchy = cv2.findContours(thresh1, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+        length = len(contours)
+        maxArea = -1
+        if length > 0:
+            for i in range(length):  # find the biggest contour (according to area)
+                temp = contours[i]
+                area = cv2.contourArea(temp)
+                if area > maxArea:
+                    maxArea = area
+                    ci = i
 
-        #     res = contours[ci]
-        #     hull = cv2.convexHull(res)
-        #     drawing = np.zeros(img.shape, np.uint8)
-        #     cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
-        #     cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
+            res = contours[ci]
+            hull = cv2.convexHull(res)
+            drawing = np.zeros(img.shape, np.uint8)
+            cv2.drawContours(drawing, [res], 0, (0, 255, 0), 2)
+            cv2.drawContours(drawing, [hull], 0, (0, 0, 255), 3)
 
-        #     isFinishCal,cnt = calculateFingers(res, drawing)                    
-        #     # print(cnt)
-        # cv2.imshow('output', drawing)
+            if count == 0:
+                isFinishCal,cnt = calculateFingers(res, drawing)                    
+                print("There are this many fingers: " + str(cnt)) # <------------- THE ANSWER
+                cv2.imshow('output', drawing)
+            
+            count = (count + 1) % 15
 
     # Keyboard OP
     k = cv2.waitKey(10)
